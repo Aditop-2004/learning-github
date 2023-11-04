98846d1980d771ee40a75b7994c30238300190f4
Aditya Chaturvedi <adityaonstudy@gmail.com>
2023-11-05 00:25:30 +0530

    first commit

---
+++ adi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define Y cout<<"YES"<<endl
+#define N cout<<"NO"<<endl
+#define int long long
+#define pb push_back
+#define ff first
+#define ss second
+using namespace std;
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+    }
+}
