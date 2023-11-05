9b289a0dc94f2f82730958e240cb6006ff6e824d
Aditya Chaturvedi <adityaonstudy@gmail.com>
2023-11-05 09:46:37 +0530

    added print statement

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define Y cout<<"YES"<<endl
+#define N cout<<"NO"<<endl
+#define int long long
+#define pb push_back
+#define ff first
+#define ss second
+using namespace std;
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int k,l;
+        cin>>k>>l;
+        cout<<"hello";
+    }
+}
